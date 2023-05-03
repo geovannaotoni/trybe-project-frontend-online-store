53a4dc01d710fd7dc472420b2e0445fe9b52e5b1
Geovanna Otoni <geovannaotoni@gmail.com>
2023-05-03 11:30:16 -0300

    req 7

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,17 +1,28 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { IconButton } from '@mui/material';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 class ProductItem extends Component {
   render() {
     const { product } = this.props;
     return (
       <>
-        <h4>{product.title}</h4>
-        <img
-          src={ product.thumbnail }
-          alt={ product.name }
-        />
-        <h4>{product.price}</h4>
+        <Link to={ `/productDetail/${product.id}` } data-testid="product-detail-link">
+          <h4>{product.title}</h4>
+          <img
+            src={ product.thumbnail }
+            alt={ product.name }
+          />
+          <h4>{product.price}</h4>
+        </Link>
+        <IconButton
+          data-testid="product-add-to-cart"
+          onClick={ () => {} }
+        >
+          <AddShoppingCartIcon />
+        </IconButton>
       </>
     );
   }
@@ -20,9 +31,10 @@ class ProductItem extends Component {
 ProductItem.propTypes = {
   product: PropTypes.shape({
     name: PropTypes.string,
-    price: PropTypes.string,
+    price: PropTypes.number,
     thumbnail: PropTypes.string,
     title: PropTypes.string,
+    id: PropTypes.string,
   }).isRequired,
 };
 
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,22 +2,55 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { IconButton } from '@mui/material';
 import { ShoppingCart } from '@mui/icons-material';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import ReplyIcon from '@mui/icons-material/Reply';
 import { Link } from 'react-router-dom';
+import { getProductById } from '../services/api';
 
 class ProductDetail extends Component {
+  state = {
+    productInfo: '',
+  };
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const productInfo = await getProductById(id);
+    this.setState({
+      productInfo,
+    });
+  }
+
   render() {
     const { history: { goBack } } = this.props;
+    const { productInfo } = this.state;
     return (
       <div>
-        <IconButton onClick={ () => goBack() }>
-          <ReplyIcon />
-        </IconButton>
-        <Link to="/cart">
-          <IconButton data-testid="shopping-cart-button">
-            <ShoppingCart />
+        <header>
+          <IconButton onClick={ () => goBack() }>
+            <ReplyIcon />
           </IconButton>
-        </Link>
+          <Link to="/cart">
+            <IconButton data-testid="shopping-cart-button">
+              <ShoppingCart />
+            </IconButton>
+          </Link>
+        </header>
+        <section>
+          <h2 data-testid="product-detail-name">{ productInfo.title }</h2>
+          <img
+            data-testid="product-detail-image"
+            src={ productInfo.thumbnail }
+            alt={ productInfo.name }
+          />
+          <h3 data-testid="product-detail-price">{ productInfo.price }</h3>
+          <h4>{ productInfo.warranty }</h4>
+          <IconButton
+            data-testid="product-add-to-cart"
+            onClick={ () => {} }
+          >
+            <AddShoppingCartIcon />
+          </IconButton>
+        </section>
       </div>
     );
   }
@@ -26,6 +59,11 @@ class ProductDetail extends Component {
 ProductDetail.propTypes = {
   history: PropTypes.shape({
     goBack: PropTypes.func,
+  }).isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
   }).isRequired,
 };
 
